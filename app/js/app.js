42e780eab67226c1047247eda27182705d8ec418
bgeeziesr <bgeeziesr@yahoo.com>
2014-04-11 15:46:52 -0500

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is up-to-date with 'origin/master'. # # Untracked files: #	.gitignore #	app/ #	bower.json #	package.json #	test/ # add bower packages for bootstrap, angular-ui-router, and jquery

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,14 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular
+    .module('myApp', ['ui.router'])
+    .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+            url: '/',
+            templateUrl: 'partials/home.html'
+        })
+    }])
