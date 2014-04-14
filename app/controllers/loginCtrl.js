bfe2c5a045da8d762109ee1940d588d0da44b4ba
Bgeeziesr <bgeeziesr@yahoo.com>
2014-04-14 03:21:40 -0500

    added ctrls and css changes

---
+++ app/controllers/loginCtrl.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+function loginCtrl($scope) {
+    $scope.loggedIn = false;
+    $scope.name = 'Brian Goodspeed';
+
+    $scope.login = function() {
+        if ($scope.username == 'test' && $scope.password == '1234') {
+            $scope.loggedIn = true;
+            $scope.username = '';
+            $scope.password = '';
+            alert("Login successful");
+        } else {
+            $scope.username = '';
+            $scope.password = '';
+            alert("Login failed. Invalid username or password");
+        }
+    };
+}
---
+++ app/controllers/menuCtrl.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+function menuCtrl($scope) {
+    $scope.links = [
+        {
+            'label': 'My PMTs',
+            'href': '/pmt',
+            'icon': 'folder-open'
+        },
+        {
+            'label': 'Logout',
+            'href': '/',
+            'icon': 'sign-out'
+        }
+    ];
+};
